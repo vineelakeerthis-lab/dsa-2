47c70d9a898a77e814432e8a59b6228b9123e235
vineelakeerthis <vineelakeerthis@gmail.com>
2026-09-25 16:21:19 +0530

    day 4 of dsa

---
+++ day1/array.py
@@ -0,0 +1,9 @@
+# Program to find sum of array elements
+#  n = int(input("Enter number of elements: ")) 
+# arr = [] 
+# for i in range(n):
+#  arr.append(int(input("Enter element: ")))
+#  sum = 0
+#  for i in arr:
+#  sum += i
+#  print("Sum of elements:", sum) 
